b5602e72fedf6aa334fe2d1a34ca8c4e9947090b
Aayush <bhaskarsharma64047@gmail.com>
2023-01-21 18:57:41 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/LazyDeveloperr/LazyPrincessV2 /LazyPrincessV2
+  git clone https://github.com/Ayush64047/MdiskPrince /MdiskPrincess
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /LazyPrincessV2
+  git clone $UPSTREAM_REPO /MdiskPrince
 fi
-cd /LazyPrincessV2
+cd /MdiskPrince
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
