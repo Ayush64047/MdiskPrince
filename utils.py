eaa20c64241facb0414172617b125e2ce59e89ce
Aayush <bhaskarsharma64047@gmail.com>
2023-01-21 20:09:31 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -383,7 +383,7 @@ async def get_shortlink(link):
         https = "https"
         link = link.replace("http", https)
 
-    url = f'https://omegalinks.in/api'
+    url = f'https://shorturllink.in'
     params = {'token': URL_SHORTNER_WEBSITE_API,
               'link': link,
               'format': 'json'
@@ -394,11 +394,13 @@ async def get_shortlink(link):
             async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
                 data = await response.json(content_type='text/html')
                 if data["status"] == "success":
-                    return data['api']
+                    return data['shortlink']
                 else:
                     logger.error(f"Error: {data['message']}")
-                    return f'https://omegalinks.in/api?token={URL_SHORTNER_WEBSITE_API}&link={link}'
+                    return = f"https://shorturllink.in/api?api={URL_SHORTNER_WEBSITE_API}&link={link}'
 
     except Exception as e:
         logger.error(e)
-        return f'https://omegalinks.in/api?token={URL_SHORTNER_WEBSITE_API}&link={link}'
+        return = f"https://shorturllink.in/api?api={URL_SHORTNER_WEBSITE_API}&link={link}'
+
+            
